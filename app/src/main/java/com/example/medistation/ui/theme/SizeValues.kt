95aa2a08035378a22e796d4027f82363ccbca24d
Dekroo <dekroostream@gmail.com>
2025-06-04 14:41:22 +0200

    New SizeValues

---
+++ app/src/main/java/com/example/medistation/ui/theme/SizeValues.kt
@@ -0,0 +1,9 @@
+package com.example.medistation.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val iconSize = 40.dp
+val headerTitle = 40.sp
+val headerDescription = 36.sp
+val musicIcon = 68.dp
